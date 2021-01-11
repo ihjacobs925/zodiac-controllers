559df1c7ffabda1c1b1ec8189f853d0deb7bd69d
ihjacobs925 <76010554+ihjacobs925@users.noreply.github.com>
2021-01-11 15:45:22 -0600

    images add

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const express = require('express');
 const app = express();
 const ejsLayouts = require('express-ejs-layouts');
 
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+
 app.get('/', (req, res) => {
     res.render('home')
   });
 
 app.get('/water', (req, res) => {
-    res.render('/water', {water: []});
+    res.render('/water', {signs: ['Pisces', 'Cancer', 'Scorpio'], traits: ['private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive']});
   });
 
   app.get('/air', (req, res) => {
-    res.render('/air', {air: []});
+    res.render('/air', {signs: ['Aquarius', 'Gemini', 'Libra'], traits: ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']});
   });
 
   app.get('/fire', (req, res) => {
-    res.render('/fire', {fire: []});
+    res.render('/fire', {signs: ['Aries', 'Leo', 'Sagittarius'], traits: ['passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting']});
   });
 
 app.get('/earth', (req, res) => {
-    res.render('/earth', {earth: []});
+    res.render('/earth', {signs: ['Taurus', 'Vergo', 'Capricorn'], traits: ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']});
   });
 
   app.listen(8005);
